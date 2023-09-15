5945a553503cd5243daf85302b13b2192ff4a829
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-09-15 13:48:17 +0700

    done basic UI

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,19 @@
-// Your JS code goes here
+const modalsElement = document.getElementById('modals');
+
+let modalOpeningClassName = '';
+
+const openModal = (modalClass) => {
+  const modalElement = document.querySelector('.' + modalClass);
+  modalOpeningClassName = modalClass;
+
+  modalsElement.classList.toggle('modal-close');
+  modalElement.classList.toggle('modal-close');
+};
+
+const closeModal = () => {
+  const modalElement = document.querySelector('.' + modalOpeningClassName);
+  if (!modalElement) return;
+
+  modalsElement.classList.toggle('modal-close');
+  modalElement.classList.toggle('modal-close');
+};
