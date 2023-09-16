5d209e26a2796f04315d3919e9ce7e8119f94139
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-09-16 20:19:49 +0700

    final touches

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -3,15 +3,34 @@ const addBookFormElement = document.getElementById('add-book-form');
 const booksTableBodyElement = document.getElementById('books-table-body');
 const deleteBookNameElement = document.getElementById('delete-book-name');
 const searchInputElement = document.getElementById('search');
+const topicSelectOptionsElement = document.querySelector(
+  '.topic-select-options'
+);
+const endBookRowElement = document.querySelector('.end-book-row');
 
 const DELETE_BOOKS_CONFIRMATION_CLASSNAME = 'delete-book-confirmation';
 
 let modalOpeningClassName = '';
 let selectedBookId = '';
 
+const dummyData = [
+  {
+    id: 'sdfjsdfklj',
+    name: 'Book 1',
+    author: 'Khoa Truong',
+    topic: 'Programming',
+  },
+  {
+    id: 'qwerqwerqwer',
+    name: 'Book 2',
+    author: 'Khoa Truong',
+    topic: 'DevOps',
+  },
+];
+
 const books = localStorage.getItem('books')
   ? JSON.parse(localStorage.getItem('books'))
-  : [];
+  : dummyData;
 
 const openModal = (modalClass) => {
   const modalElement = document.querySelector('.' + modalClass);
@@ -71,8 +90,8 @@ const deleteBookById = () => {
 const addBookRowHTML = (book, order) => {
   const bookRowHTML = `<tr>
   <td>${order}</td>
-  <td>${book.name}</td>
-  <td>${book.author}</td>
+  <td class="book-name">${book.name}</td>
+  <td class="book-author">${book.author}</td>
   <td>${book.topic}</td>
   <td class="book-actions" onclick=openDeleteBookModal('${book.id}')>Delete</td>
 </tr>`;
@@ -97,6 +116,11 @@ const addBook = (book) => {
   saveBooksToLocalStorage();
   addBookRowHTML(book, books.length);
   closeModal();
+  searchBooks();
+
+  const topicSelectedElement = document.querySelector('.topic-select');
+  topicSelectedElement.innerText = 'Programming';
+  endBookRowElement.scrollIntoView();
 };
 
 addBookFormElement.addEventListener('submit', (e) => {
@@ -107,10 +131,11 @@ addBookFormElement.addEventListener('submit', (e) => {
   const bookTopicInput = document.getElementById('book-topic');
 
   addBook({
-    id: bookAuthorInput.value + bookNameInput.value + Math.random(),
+    id:
+      bookAuthorInput.value.trim() + bookNameInput.value.trim() + Math.random(),
     name: bookNameInput.value,
     author: bookAuthorInput.value,
-    topic: bookTopicInput.value,
+    topic: bookTopicInput.value === '' ? 'Programming' : bookTopicInput.value,
   });
 });
 
@@ -128,6 +153,10 @@ const debounce = (func, delay = 100) => {
 
 const searchBooks = () => {
   const searchValue = searchInputElement.value.toLowerCase();
+  if (!searchValue) {
+    renderBookRows(books);
+    return;
+  }
 
   const searchedBooks = books.filter(
     (book) =>
@@ -142,5 +171,45 @@ const onSearchInput = debounce(searchBooks, 500);
 searchInputElement.addEventListener('input', () => {
   onSearchInput();
 });
+
+const toggleOpenTopicSelectOptions = () => {
+  const bookTopicInput = document.getElementById('book-topic');
+  const bookTopicValue =
+    bookTopicInput.value !== '' ? bookTopicInput.value : 'Programming';
+
+  topicSelectOptionsElement.classList.toggle('select-close');
+
+  if (topicSelectOptionsElement.className.includes('select-close')) return;
+
+  const topicOptionsElement = document.querySelectorAll(
+    '.topic-select-options li'
+  );
+  topicOptionsElement.forEach((option) => {
+    if (option.innerText === bookTopicValue) {
+      option.classList.add('topic-selected');
+    } else option.classList.remove('topic-selected');
+  });
+};
+
+const selectTopicOption = (topic) => {
+  const bookTopicInput = document.getElementById('book-topic');
+  const topicSelectedElement = document.querySelector('.topic-select');
+  bookTopicInput.value = topic;
+  toggleOpenTopicSelectOptions();
+  topicSelectedElement.innerText = topic;
+};
+
+const onClickOutside = (e) => {
+  if (
+    !e.target.className.includes('topic-select-trigger') &&
+    !e.target.className.includes('topic-select') &&
+    !e.target.className.includes('topic-select-options') &&
+    !topicSelectOptionsElement.classList.contains('select-close')
+  ) {
+    topicSelectOptionsElement.classList.add('select-close');
+  }
+};
+
+window.addEventListener('click', onClickOutside);
 
 renderBookRows(books);
