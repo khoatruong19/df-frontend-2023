9581b3d0e7c8b29c7b6fb188c0e90ee89e15aaa7
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-09-27 13:55:52 +0700

    test eslint check before commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">let asdhfksdhfjk</header>
+      <header className="App-header">let asdhfksdhfjk 23</header>
     </div>
   )
 }
